041775569e7178ab2b78ce79807f0b9ce8787af9
Anushka Singh Raghuvanshi <114311721+Theanushkaraghuvanshi@users.noreply.github.com>
2023-10-12 23:48:43 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,24 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"hello world";
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int sum = 6;
+    cout<< "hello world" << sum;
+    return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a =2;
+    int b =3;
+    cout<<"this is tutorial 4. here the value of a is "<<a<<". the value of b is "<<b<<".";
+    return 0;
+}
